66c14577853d5cde780c8cf8c1dfa810a5bd0563
Yashwanth Nadella <61127833+ynadell@users.noreply.github.com>
2025-03-07 12:20:22 -0500

    changed repo name

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ynadell.github.io/', // This is important for GitHub Pages
+  base: '/ynadell.github.io/ynadell', // This is important for GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 }) 
