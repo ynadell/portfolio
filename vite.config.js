29e5c3635147c8b118482d29276186c4defddce4
Yashwanth Nadella <61127833+ynadell@users.noreply.github.com>
2025-03-07 13:14:29 -0500

    chaned repo name to portfolio

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ynadell.github.io/',
+  base: '/portfolio/',
   plugins: [react()],
   build: {
     outDir: 'dist',
